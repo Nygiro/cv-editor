aa576d66258ed7b383f63c313564fb2eaa66f6e5
nigirollup <nigirollup@gmail.com>
2020-04-22 17:26:49 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>CV Florian Fremin</title>
+</head>
+<body>
+<div class="content">
+    <div class="left">
+        <img src="./img/logo_fremin.png" alt="">
+    </div>
+    <div class="right">
+        <pre>
+            <span>{</span>
+            <span><span class="dot-space">..</span><span class="key">name</span>: "<span
+                    class="value">Fremin Florian</span>",</span>
+            <span><span class="dot-space">..</span><span
+                    class="key">job</span>:  "<span class="value">Développeur back-end</span>",</span>
+            <span></span>
+            <span><span class="dot-space">..</span><span class="key">email</span>:    "<span class="value">florian.fremin33@gmail.com</span>",</span>
+            <span><span class="dot-space">..</span><span class="key">location</span>: "<span class="value">Bordeaux, France</span>",</span>
+            <span></span>
+                           <span><span class="dot-space">..</span><span class="key">skills:</span> {</span>
+            <span><span class="dot-space">...</span>"<span class="key-orange">front</span>": ["<span class="value">HTML</span>", "<span class="value">CSS</span>", "<span class="value">Javascript</span>"],</span>
+            <span><span class="dot-space">...</span>"<span class="key-orange">back</span>": ["<span class="value">PHP</span>", "<span class="value">Ruby</span>", "<span class="value">Mysql</span>"],</span>
+            <span><span class="dot-space">...</span>"<span class="key-orange">infra</span>": ["<span class="value">Docker</span>", "<span class="value">Apache</span>"],</span>
+            <span><span class="dot-space">...</span>"<span class="key-orange">framework</span>": ["<span class="value">Symfony</span>", "<span class="value">React</span>", "<span class="value">Vue</span>", "<span class="value">Wordpress</span>", "<span class="value">Prestashop</span>"]</span>
+            <span><span class="dot-space">..</span>},</span>
+                        <span></span>
+            <span><span class="dot-space">..</span><span class="key">parcours:</span> {</span>
+            <span><span class="dot-space">...</span>"<span class="key-orange">2018</span>-<span
+                    class="key-orange">2020</span>": "<span
+                    class="value">Mastère Développement Web</span> @ <span class="value">ECV</span>",</span>
+            <span><span class="dot-space">...</span>"<span class="key-orange">2017</span>-<span
+                    class="key-orange">2018</span>": "<span
+                    class="value">Licence Développeur Web</span> @ <span class="value">Université de La Rochelle</span>",</span>
+                       <span><span class="dot-space">...</span>"<span class="key-orange">2016</span>-<span
+                               class="key-orange">2017</span>": "<span
+                               class="value">Formation Développeur Web</span> @ <span
+                               class="value">Le wagon</span>",</span>
+                                   <span><span class="dot-space">...</span>"<span class="key-orange">2014</span>-<span
+                                           class="key-orange">2016</span>": "<span
+                                           class="value">BTS Management des unités commerciales</span> @ <span
+                                           class="value">Elie Faure</span>",</span>
+                                               <span><span class="dot-space">...</span>"<span
+                                                       class="key-orange">2013</span>-<span
+                                                       class="key-orange">2014</span>": "<span
+                                                       class="value">BTS Services informatiques aux organisations</span> @ <span
+                                                       class="value">Briffaut</span>",</span>
+                                                          <span><span class="dot-space">...</span>"<span
+                                                                  class="key-orange">2010</span>-<span
+                                                                  class="key-orange">2013</span>": "<span
+                                                                  class="value">BAC Sciences et technologies de la gestion</span> @ <span
+                                                                  class="value">Jaufré Rudel</span>"</span>
+            <span><span class="dot-space">..</span>},</span>
+                        <span></span>
+               <span><span class="dot-space">..</span><span class="key">experiences:</span> {</span>
+            <span><span class="dot-space">...</span>"<span class="key-orange">2018</span>-<span
+                    class="key-orange">20??</span>": "<span
+                    class="value">Développeur web</span> @ <span class="value">TimeOne</span>",</span>
+                        <span><span class="dot-space">...</span>"<span class="key-orange">2018</span>-<span
+                                class="key-orange">2018</span>": "<span
+                                class="value">Développeur web</span> @ <span class="value">Definima</span>",</span>
+                                    <span><span class="dot-space">...</span>"<span class="key-orange">2017</span>-<span
+                                            class="key-orange">2018</span>": "<span
+                                            class="value">Développeur web</span> @ <span class="value">Patch.ek</span>",</span>
+                                                <span><span class="dot-space">...</span>"<span class="key-orange">2016</span>-<span
+                                                        class="key-orange">2016</span>": "<span
+                                                        class="value">Technicien/Vendeur informatique</span> @ <span class="value">Easy Cash</span>"</span>
+            <span><span class="dot-space">..</span>},</span>
+                        <span></span>
+            <span><span class="dot-space">..</span><span class="key">links:</span> {</span>
+            <span><span class="dot-space">...</span>"<span class="key-orange">portfolio</span>": "<span
+                    class="value"><a href="#">florian-fremin.fr</a></span>",</span>
+                        <span><span class="dot-space">...</span>"<span class="key-orange">github</span>":    "<span
+                                class="value"><a href="https://github.com/nigirollup" target="_blank">github.com/nigirollup</a></span>",</span>
+                        <span><span class="dot-space">...</span>"<span class="key-orange">linkedin</span>":  "<span
+                                class="value"><a href="https://fr.linkedin.com/in/freminflorian" target="_blank">linkedin/florianfremin</a></span>",</span>
+                                    <span><span class="dot-space">...</span>"<span class="key-orange">codepen</span>":   "<span
+                                            class="value"><a
+                                            href="https://codepen.io/nigirollup" target="_blank">codepen.io/nigirollup</a></span>"</span>
+            <span><span class="dot-space">..</span>}</span>
+              <span>}</span>
+        </pre>
+    </div>
+</div>
+</body>
+</html>
